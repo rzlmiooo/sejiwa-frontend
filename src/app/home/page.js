f22a13ff5c9a6e52755ab988afac773a9bd0c026
rzlmiooo <zrrizall10@example.com>
2025-06-02 22:59:00 +0700

    landing page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Bento from "../components/bento"
 
 let username = 'Rizal'
 
@@ -11,35 +11,7 @@ export default function Home(){
             <div className="text-gray-900 text-lg font-medium">
                 Temukan ketenangan jiwamu.
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-4 w-full h-full">
-
-            {/* <!-- Bento 1 --> */}
-            <Link href="/home/assessment" className="bg-white rounded-l-4xl shadow-md p-10 flex flex-col items-start space-y-2 border-t-2 border-t-blue-200 hover:scale-101">
-              <h2 className="text-xl font-bold text-gray-900">Assessment</h2>
-              <p className="text-sm text-gray-600">Survei singkat untuk mengetahui isi hatimu.</p>
-              <div className="mt-10 flex justify-center items-center">
-                <img src="/assessment.png" alt="Image 3" className="w-32 h-auto object-cover rounded-xl"/>
-              </div>
-            </Link>
-                
-              {/* <!-- Bento 2 --> */}
-            <Link href="/home/recommendation" className="bg-white shadow-md p-10 flex flex-col items-start space-y-2 border-t-2 border-t-blue-200 hover:scale-101">
-                <h2 className="text-xl font-bold text-gray-900">Rekomendasi</h2>
-                <p className="text-sm text-gray-600">Video dan artikel yang Sejiwa rekomendasikan khusus untukmu.</p>
-                <div className="mt-10 flex justify-center items-center">
-                  <img src="/like.png" alt="Image 3" className="w-32 h-auto object-cover rounded-xl"/>
-                </div>
-            </Link>
-                
-              {/* <!-- Bento 3 --> */}
-            <Link href="/home/chat" className="bg-white rounded-r-4xl shadow-md p-10 flex flex-col items-start space-y-2 border-t-2 border-t-blue-200 hover:scale-101">
-              <h2 className="text-xl font-bold text-gray-900">Chat</h2>
-              <p className="text-sm text-gray-600">Pengalaman konseling terbaik dari para konselor profesional. Gratis.</p>
-              <div className="mt-12 flex justify-center items-center">
-                <img src="/chat.png" alt="Image 3" className="w-32 h-auto object-cover rounded-xl"/>
-              </div>
-            </Link>
-            </div>
+            <Bento />
         </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { ArrowUpRightIcon, Bars3Icon, XMarkIcon, HandThumbUpIcon } from '@heroicons/react/24/solid'
+import Bento from './components/bento'
 import Link from 'next/link'
 
 const navigation = [
@@ -132,7 +133,7 @@ export default function LandingPage() {
             </p>
             <div className="mt-10 flex items-center justify-center md:justify-start gap-x-6">
               <Link
-                href="#"
+                href="/home/assessment"
                 className="flex items-center justify-center z-50 rounded-4xl bg-sky-600 px-5 py-2.5 text-lg font-semibold text-white shadow-xs hover:bg-sky-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
               >
                 Assessment<ArrowUpRightIcon className="ml-2 size-5"/>
@@ -161,9 +162,9 @@ export default function LandingPage() {
       <div className="relative md:-top-10 z-0">
         <div className="absolute -z-9999 -top-10 md:-top-10 -left-60 lg:-left-50 m-0 w-80 md:w-100 h-80 md:h-100 rounded-full bg-sky-500"></div>
         
-        <div className="mb-20 flex items-center justify-center text-5xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
-          Fitur Utama
-          {/* <div className="absolute top-7 left-80 -z-10 w-20 h-5 rounded-full bg-sky-500"/> */}
+        <div className="mb-10 sm:mb-20 flex items-center justify-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
+          Tujuan Layanan
+          <div className="absolute top-7 left-18 sm:left-105 -z-10 w-10 sm:w-20 h-3 sm:h-5 rounded-full bg-sky-500"/>
         </div>
 
         <div className="relative flex flex-wrap align-center justify-center z-10 m-5 gap-5 md:gap-10">
@@ -193,46 +194,77 @@ export default function LandingPage() {
           </div>
         </div>
 
-        <div className="absolute top-50 -right-50 m-0 w-80 md:w-100 h-80 md:h-100 rounded-full bg-sky-500 overflow-x-hidden-hidden"></div>
+        <div className="absolute top-50 -right-50 m-0 w-80 md:w-100 h-80 md:h-100 rounded-full bg-sky-700 overflow-x-hidden"></div>
       </div>
       <button onClick={() => setMobileMenuOpen(true)} className="fixed md:hidden h-15 sm:h-20 w-15 sm:w-20 rounded-full z-50 bottom-6 right-6 bg-gray-900">
         <img src="/favicon.ico"/>
       </button>
+      
+      {/* fitur */}
+      <div className="m-10 flex flex-col justify-items-center">
+         <div className="mt-20 mb-10 sm:mb-20 flex items-center justify-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
+            Fitur Utama
+         </div>
+        <Bento />
+      </div>
+
+      {/* bottom-cta */}
+      <div className="mx-100 my-50 flex flex-col justify-center items-center">
+        <div className="mt-20 mb-20 sm:mb-20 flex justify-center text-center text-4xl text-gray-950 sm:text-5xl">
+          Sejiwa bersedia mengantarmu menuju dirimu yang lebih baik. 
+        </div>
+        <Link href="/home/assessment" className="mt-0 mx-20 mb-10 flex items-center justify-center z-50 rounded-4xl bg-sky-600 px-5 py-2.5 text-lg font-semibold text-white shadow-xs hover:bg-sky-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
+          Gabung Sekarang<ArrowUpRightIcon className="ml-2 size-5"/>
+        </Link>
+        <div className="-left-50 absolute m-0 w-80 md:w-100 h-80 md:h-100 rounded-full bg-sky-700 overflow-x-hidden"></div>
+        <div className="absolute -right-50 m-0 w-80 md:w-100 h-80 md:h-100 rounded-full bg-sky-500"></div>
+      </div>
 
       {/* footer */}
       <div className="bg-gray-800 text-white py-10 mt-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-row items-center justify-center">
-            <div className="flex flex-col items-center justify-center">
-              <img src="/icon.png" alt="Logo" className="h-16 w-auto mb-4" />
-              <p className="text-center text-sm">
-                &copy; {new Date().getFullYear()} Sejiwa. All rights reserved.
-              </p>
-              <div className="mt-4 flex space-x-4">
-                <Link href="/privacy" className="text-sm hover:underline">Privacy Policy</Link>
-                <Link href="/terms" className="text-sm hover:underline">Terms of Service</Link>
+          <div className="flex flex-row items-start justify-center gap-10">
+            <div className="flex flex-col items-start justify-center gap-7">
+              <img src="/icon.png" alt="Logo" className="h-10 w-auto mb-4" />
+              <div>
+                <p className="text-center text-sm">
+                  &copy; {new Date().getFullYear()} Sejiwa. All rights reserved.
+                </p>
+                <div className="mt-2 flex space-x-4">
+                  <Link href="/privacy" className="text-sm hover:underline">Privacy Policy</Link>
+                  <Link href="/terms" className="text-sm hover:underline">Terms of Service</Link>
+                </div>
               </div>
             </div>
-              <div className="flex flex-col items-center justify-center ml-10">
-              <h2 className="text-lg font-semibold mb-2">Sejiwa</h2>
-              <div className='flex flex-col items-center justify-center'>
-                <Link href="/home" className="text-white hover:text-gray-400">Home</Link>
-                <Link href="/assessment" className="text-white hover:text-gray-400">Assessment</Link>
-                <Link href="/about" className="text-white hover:text-gray-400">About</Link>
-                <Link href="/contact" className="text-white hover:text-gray-400">Contact</Link>
+              <div className="flex flex-col items-center justify-start ml-10">
+                <h2 className="text-lg font-semibold mb-3">Sejiwa App</h2>
+                <div className='pl-4 flex flex-col items-start justify-center'>
+                  <Link href="/home" className="text-white hover:text-gray-400">Home</Link>
+                  <Link href="/assessment" className="text-white hover:text-gray-400">Assessment</Link>
+                  <Link href="/about" className="text-white hover:text-gray-400">About</Link>
+                  <Link href="/contact" className="text-white hover:text-gray-400">Contact</Link>
+                </div>
               </div>
-              </div>
-              <div className="flex flex-col items-center justify-center ml-10">
-              <h2 className="text-lg font-semibold mb-2">Follow Us</h2>
-              <div className="flex flex-col space-x-4">
-                <Link href="#" className="text-white hover:text-gray-400">
-                  <img src="/icons/facebook.svg" alt="Facebook" className="h-6 w-6"/>
+              <div className="flex flex-col items-start justify-center ml-10">
+              <h2 className="text-lg font-semibold mb-3">Follow Us</h2>
+              <div className="pl-4 flex flex-col space-x-4">
+                <Link href="#" className="flex gap-2 text-white hover:text-gray-400">
+                <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
+                </svg>
+                  <p>Sejiwa</p>
                 </Link>
-                <Link href="#" className="text-white hover:text-gray-400">
-                  <img src="/icons/twitter.svg" alt="Twitter" className="h-6 w-6"/>
+                <Link href="#" className="flex gap-2 text-white hover:text-gray-400">
+                  <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                  </svg>
+                  <p>sejiwa.app</p>
                 </Link>
-                <Link href="#" className="text-white hover:text-gray-400">
-                  <img src="/icons/instagram.svg" alt="Instagram" className="h-6 w-6"/>
+                <Link href="#" className="flex gap-2 text-white hover:text-gray-400">
+                  <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                  <path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" />
+                  </svg>
+                  <p>sejiwa.app</p>
                 </Link>
               </div>
             </div>
