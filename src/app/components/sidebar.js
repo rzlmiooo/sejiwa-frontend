ac450d7bcff1dbf632c6910778372d66b586a823
rzlmiooo <zrrizall10@example.com>
2025-07-17 14:53:31 +0700

    wkwkw

--- src/app/components/sidebar.js
+++ src/app/components/sidebar.js
@@ -3,13 +3,14 @@
 import { useState } from 'react';
 import Link from 'next/link'
 import { AuthService } from '../service/AuthService';
+import Image from 'next/image';
 
 const navigation = [
-    {src: '/dashboard.png', name: 'Beranda', href: '/home' },
-    {src: '/assessment.png', name: 'Assessment', href: '/home/assessment' },
-    {src: '/like.png', name: 'Rekomendasi', href: '/home/recommendation' },
-    {src: '/chat.png', name: 'Chat Konselor', href: '/home/chat' },
-    {src: '/settings.png', name: 'Pengaturan', href: '/home/settings' }
+    {src: '/dashboard.png', alt: "icon", name: 'Beranda', href: '/home' },
+    {src: '/assessment.png', alt: "icon", name: 'Assessment', href: '/home/assessment' },
+    {src: '/like.png', alt: "icon", name: 'Rekomendasi', href: '/home/recommendation' },
+    {src: '/chat.png', alt: "icon", name: 'Chat Konselor', href: '/home/chat' },
+    {src: '/settings.png', alt: "icon", name: 'Pengaturan', href: '/home/settings' }
 ]
 
 export default function Sidebar({ isOpen }) {
@@ -36,7 +37,7 @@ export default function Sidebar({ isOpen }) {
         >
             {navigation.map((item) => (
                 <Link key={item.name} href={item.href} className="mx-5 my-8 flex flex-row items-center gap-5 hover:scale-102">
-                    <img src={item.src} className='size-5 invert dark:invert-0'></img>{ isOpen && <h1 className="p-0 text-sm font-bold text-sky-900 dark:text-sky-50">{item.name}</h1> }
+                    <Image src={item.src} alt={item.alt} width={50} height={50} className='size-5 invert dark:invert-0'></Image>{ isOpen && <h1 className="p-0 text-sm font-bold text-sky-900 dark:text-sky-50">{item.name}</h1> }
                 </Link>
             ))}
         </div>
