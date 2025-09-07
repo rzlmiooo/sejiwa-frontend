7b06cf75a83ffea890da1383262a6da3eeca5944
rzlmiooo <zrrizall10@example.com>
2025-09-07 16:05:52 +0700

    seo fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,8 @@ import "./globals.css";
 import AuthRedirect from "./utils/auth/redirect";
 
 export const metadata = {
-  title: "Welcome to Sejiwa",
-  description: "Platform Konseling Digital",
+  title: "Sejiwa App | PLatform Konseling Digital",
+  description: "Dengan pendekatan postmodern, SEJIWA menawarkan pengalaman konseling yang interaktif dan personal, berfokus pada mindfulness, refleksi diri, dan keseimbangan emosional"
 };
 
 export default function RootLayout({ children }) {
